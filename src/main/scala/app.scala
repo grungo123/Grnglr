339fe858dbaebac0c005d3a7158bc3bbfbc6e1eb
Matias Piironen <matias.piironen@aalto.fi>
2025-08-22 21:47:43 +0300

    Initial commit

---
+++ src/main/scala/app.scala
@@ -0,0 +1,62 @@
+import scalafx.application.JFXApp3
+import scalafx.scene.*
+import scalafx.scene.control.Label
+
+
+
+
+
+
+
+
+
+
+
+
+object demo extends JFXApp3:
+  /**Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Aliquam ut iaculis tellus. Nunc pellentesque est leo, nec mollis massa iaculis eu.
+ * Nunc gravida diam a sem accumsan, vitae bibendum nunc ullamcorper.
+ * Ut tincidunt sapien a finibus varius.
+ * Aliquam aliquet, lacus sit amet imperdiet laoreet, enim magna rutrum leo, ac pharetra risus eros eu nunc.
+ * Sed dignissim, purus ut aliquam bibendum, justo lectus efficitur mauris, eget maximus leo augue consequat ex.
+ * Sed vulputate risus ut sapien ullamcorper ultricies non vel ex.
+ * Mauris pulvinar, massa sed rutrum elementum, magna tellus tincidunt nisl, at faucibus sapien lacus quis nunc.
+ * Nam ac malesuada justo. Proin hendrerit viverra massa ac viverra. Integer pharetra at urna ac rhoncus.
+ * Nulla at ligula mauris. Donec mollis dictum varius.
+ * Duis rhoncus, metus sed tincidunt venenatis, felis massa feugiat turpis, a faucibus tellus nisi a orci.
+
+Suspendisse sit amet lobortis mi. Ut eleifend sodales ligula, sit amet lacinia mauris eleifend ut.
+ Interdum et malesuada fames ac ante ipsum primis in faucibus.
+ Maecenas elementum orci laoreet, volutpat urna quis, facilisis lacus.
+ In ac elit in nibh vulputate laoreet. Donec lacinia ligula ex, vitae venenatis tortor accumsan sed.
+ Proin id turpis elit. Vivamus posuere urna a felis lacinia, eget euismod nulla posuere.
+ Duis tempus mattis nibh, vel consectetur augue.
+ Fusce vulputate orci quis nisl rutrum, quis egestas ligula venenatis.
+ Nunc quis auctor purus, eget ullamcorper purus. Ut viverra molestie malesuada.
+
+Quisque condimentum venenatis mi vitae elementum.
+ Quisque vel porta nisi. Donec et nisi nisi.
+ Morbi mollis nunc nec erat dignissim aliquet.
+ Suspendisse volutpat nisi lectus, ac lobortis turpis ultrices eu.
+ Nulla rhoncus molestie nunc ut ultricies. Nunc sit amet pharetra nisi.
+ Morbi laoreet volutpat venenatis.
+ Aenean scelerisque ligula sapien, eu viverra ipsum condimentum eget.
+ Donec at risus at arcu ornare feugiat in vel nibh.
+ Maecenas bibendum posuere lacus, eu malesuada metus tempor sit amet.
+ Aliquam ut gravida urna.
+ Suspendisse potenti.
+ Cras pulvinar dolor id ligula egestas molestie.
+*/
+
+  def start() =
+    stage = new JFXApp3.PrimaryStage:
+      title = "Grnglr"
+      width = 100
+      height = 65
+
+    val root = new Label("Get rugpulled")
+
+    val scene = Scene(parent = root)
+
+    stage.scene = scene
